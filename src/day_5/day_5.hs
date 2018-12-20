1e32f7318ad7447861f5fede42e07eb52e0e4c56
phip123 <poepsi39@gmail.com>
2018-12-20 09:25:31 +0100

    day 5 part 2

--- src/day_5/day_5.hs
+++ src/day_5/day_5.hs
@@ -6,6 +6,8 @@ where
 import           Data.Char
 
 rangeBetweenUpperAndLower = 32
+start = 65
+end = 90
 
 isReactivePair :: Char -> Char -> Bool
 isReactivePair c1 c2 = abs (ord c1 - ord c2) == rangeBetweenUpperAndLower
@@ -23,7 +25,11 @@ reduceAll reduced original | reduced == original = reduced
 reduce :: String -> String
 reduce s = reduceAll (reduceOnce s) s
 
+removeAll :: Char -> String -> String
+removeAll c = filter (\x -> toUpper x /= toUpper c)
+
 solve :: String -> IO ()
 solve fileName = do
     content <- readFile fileName
     print $ (length . reduce) content
+    print $ minimum $ map (\c -> ((length . reduce . removeAll (chr c)) content, chr c)) [start..end]
